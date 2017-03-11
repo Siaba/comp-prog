367b72b2bc898e16eb6d2771532cb4f7edc214c1
spacedmaster <spacedmaster@gmail.com>
2017-03-11 18:50:21 -0500

    Update app.js

--- codechamps/app.js
+++ codechamps/app.js
@@ -117,7 +117,7 @@ app.post('/blanktext.html', function(req, res){
 				 console.error("javac failed");
 				 return;
 			 }
-			 exec('isolate --processes=15 --run -- java test', (error,stdout,stderr) => {
+			 exec('isolate --processes=15 --stdout=output.txt --run -- java test', (error,stdout,stderr) => {
 				 if(error) {
 					 console.error("test run failed");
 					 return;
