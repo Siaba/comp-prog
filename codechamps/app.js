beabe30b26fb29e0ae8ec6ad8a8653b673adef48
Siaba <siaba9@live.com>
2017-03-28 19:28:12 -0400

    Update app.js

--- codechamps/app.js
+++ codechamps/app.js
@@ -67,7 +67,7 @@ app.get('/Account.html', function(req, res){
       
     req.session.views++;
 	console.log("The cookie information is: " + "\n" + req.cookies);
-	console.log("The session ID for the user is: " + "\n" + req.sessionID);
+	console.log("The session ID for " + req.session.user_name + " is: " + "\n" + req.sessionID);
     console.log("Visited the page" + req.session.views);
 
    }
@@ -77,13 +77,13 @@ app.get('/Account.html', function(req, res){
       req.session.views = 1;
       console.log('Visited the home page for the first time');
 	  console.log("The cookie information is: " + "\n" + req.cookies);
-	  console.log("The session ID for the user is: " + "\n" + req.sessionID);
+	  console.log("The session ID for " + req.session.user_name + " is: " + "\n" + req.sessionID);
    }
 
    console.log(req.session.views);
 	console.log('Visited the home page for the first time');
 	console.log("The cookie information is: " + "\n" + req.cookies);
-	console.log("The session ID for the user is: " + "\n" + req.sessionID);
+	console.log("The session ID for " + req.session.user_name + " is: " + "\n" + req.sessionID);
    	res.sendFile(path.join(__dirname + '/webapp/Account.html'));
 });
 
