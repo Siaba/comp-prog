a8132ff786ff1c820e140b45164f665f764af7f0
GriffTheMyth <gkepp13@gmail.com>
2017-03-29 00:14:21 -0400

    Update index.js

--- codechamps/routes/index.js
+++ codechamps/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+ res.sendFile(path.join(__dirname + '/webapp/Home.html'));
 });
 
 module.exports = router;
