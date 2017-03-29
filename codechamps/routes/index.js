d61b90d04f2b9a9269abfab2bedf387142156104
GriffTheMyth <gkepp13@gmail.com>
2017-03-29 00:53:44 -0400

    Update index.js

--- codechamps/routes/index.js
+++ codechamps/routes/index.js
@@ -12,79 +12,79 @@ router.get('/', function(req, res) {
  
 });
 
-router.get('/', function(req, res) {
+router.get('/Account.html', function(req, res) {
  
  console.log("ROUTES WORKED: Account Page");
  res.sendFile(path.join(filePath + '/webapp/Account.html'));
  
 });
 
-router.get('/', function(req, res) {
+router.get('/CreateAccount.html', function(req, res) {
  
  console.log("ROUTES WORKED: Create Account");
  res.sendFile(path.join(filePath + '/webapp/CreateAccount.html'));
  
 });
 
-router.get('/', function(req, res) {
+router.get('/PracticeMode.html', function(req, res) {
  
- console.log("ROUTES WORKED");
+ console.log("ROUTES WORKED: PracticeMode");
  res.sendFile(path.join(filePath + '/webapp/PracticeMode.html'));
  
 });
 
-router.get('/', function(req, res) {
+router.get('/Versus.html', function(req, res) {
  
- console.log("ROUTES WORKED");
+ console.log("ROUTES WORKED: versus Match");
  res.sendFile(path.join(filePath + '/webapp/Versus.html'));
  
 });
 
-router.get('/', function(req, res) {
+router.get('/Stats.html', function(req, res) {
  
- console.log("ROUTES WORKED");
+ console.log("ROUTES WORKED: stats");
  res.sendFile(path.join(filePath + '/webapp/Stats.html'));
  
 });
 
-router.get('/', function(req, res) {
+router.get('/History.html', function(req, res) {
  
- console.log("ROUTES WORKED");
+ console.log("ROUTES WORKED: history page");
  res.sendFile(path.join(filePath + '/webapp/History.html'));
  
 });
 
-router.get('/', function(req, res) {
+router.get('/Leaderboards.html', function(req, res) {
  
- console.log("ROUTES WORKED");
+ console.log("ROUTES WORKED: leaderboards");
  res.sendFile(path.join(filePath + '/webapp/Leaderboards.html'));
  
 });
 
-router.get('/', function(req, res) {
+router.get('/Contact.html', function(req, res) {
  
- console.log("ROUTES WORKED");
- res.sendFile(path.join(filePath + '/webapp/.html'));
+ console.log("ROUTES WORKED: contact us ");
+ res.sendFile(path.join(filePath + '/webapp/Contact.html'));
  
 });
 
-router.get('/', function(req, res) {
+router.get('/GroupLeaderboards.html, function(req, res) {
  
- console.log("ROUTES WORKED");
+ console.log("ROUTES WORKED: group LBS");
  res.sendFile(path.join(filePath + '/webapp/GroupLeaderboards.html'));
  
 });
 
-router.get('/', function(req, res) {
+router.get('/GroupStats', function(req, res) {
  
- console.log("ROUTES WORKED");
+ console.log("ROUTES WORKED: group stats");
  res.sendFile(path.join(filePath + '/webapp/GroupStats.html'));
  
 });
 
-router.get('/', function(req, res) {
+router.get('/FindMatch.html', function(req, res) {
  
- console.log("ROUTES WORKED");
+ console.log("ROUTES WORKED: find match");
  res.sendFile(path.join(filePath + '/webapp/FindMatch.html'));
  
 });
