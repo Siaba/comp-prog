71fce7c9221c454e334ba5787c9aca3f143c736f
spacedmaster <spacedmaster@gmail.com>
2017-03-29 00:29:06 -0400

    Update index.js

--- codechamps/routes/index.js
+++ codechamps/routes/index.js
@@ -2,10 +2,12 @@ var express = require('express');
 var path = require('path');
 var router = express.Router();
 
+var filePath = '/home/ubuntu/codechamps';
+
 /* GET home page. */
 router.get('/', function(req, res) {
  console.log("ROUTES WORKED");
- res.sendFile(path.join(__dirname + '/webapp/Home.html'));
+ res.sendFile(path.join(filePath + '/webapp/Home.html'));
 });
 
 module.exports = router;
