1bee92163a60796fd4f7d58e731e70b25a8e4272
GriffTheMyth <gkepp13@gmail.com>
2017-03-30 00:21:45 -0400

    Update index.js

--- codechamps/routes/index.js
+++ codechamps/routes/index.js
@@ -2,13 +2,24 @@ var express = require('express');
 var path = require('path');
 var router = express.Router();
 var bodyParser = require('body-parser');
+var AWS = require('aws-sdk');
+var favicon = require('static-favicon');
+var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
+var fs = require('fs');
 
 router.use(cookieParser());
 router.use(bodyParser.urlencoded({extended: true }));
 router.use(bodyParser.json());
 router.use(session({secret: "123abcg"}));
+
+AWS.config.update({
+    region: "us-east-1",
+    endpoint: "dynamodb.us-east-1.amazonaws.com"
+});
+
+
 
 var filePath = '/home/ubuntu/codechamps';
 
@@ -17,6 +28,7 @@ router.get('/', function(req, res) {
  
  console.log("ROUTES WORKED");
  var username = req.session.username;
+ var password = req.session.password;
  console.log("The username is: " + username);
  res.sendFile(path.join(filePath + '/webapp/Home.html'));
  
@@ -36,30 +48,15 @@ console.log("The username is: " + username);
  }
  
  else {
-  
-  if(session.views){
-   console.log("The " + session.views + " times youve viewed the page");
-   session.views = session.views + 1;
-  }
-  
-  else {
-   session.views = 1;
-   console.log("The first time youve viewd: view count is: " + session.views);
- }
- 
- //res.sendFile(path.join(filePath + '/webapp/CreateAccount.html'));
  
  res.sendFile(path.join(filePath + '/webapp/Account.html'));
  console.log("The cookie ID" + req.cookie);
  console.log("The SessionID" + req.sessionID);
  console.log("ROUTES WORKED: Account Page");
  
- 
- 
-}});
+});
 
-//log user in
-/*router.post('/Account.html', function(req, res) {
+router.post('/Account.html', function(req, res) {
 	var table = "user";
 	var username = req.body.user_name;
 	
@@ -98,8 +95,76 @@ console.log("The username is: " + username);
 		}
 	});
 });
-*/
 
+//add new user to database
+app.post('/CreateAccount.html', function(req, res) {
+	var table = "user";
+	var username = req.body.user_name;
+	var age = req.body.user_age;
+	var email = req.body.user_email;
+	var firstname = req.body.first_name;
+	var lastname = req.body.last_name;
+	var gender = req.body.gender;
+	var occupation = req.body.user_occupation;
+	
+	var db = new AWS.DynamoDB();
+	
+	console.log(username);
+	var paramsgetuser = {
+		TableName:table,
+		Key : {"username" : {S: username}},
+		AttributesToGet: [ "username" ]
+	};
+	
+	//check if username exists in database
+	db.getItem(paramsgetuser, function(err, data){
+		if(err) {
+			console.log(err);
+		}
+		else {
+			console.log(data);
+			if(data.Item == null){ //add user if non-existent
+				var docClient = new AWS.DynamoDB.DocumentClient();
+				
+				const saltRounds = 10;
+				bcryptjs.genSalt(saltRounds, function(err, salt) {
+					bcryptjs.hash(req.body.user_password, salt, function(err, hash){
+						//user tuple
+						var params = {
+							TableName:table,
+							Item:{
+								"username": username,
+								"age": age,
+								"email": email,
+								"firstname": firstname,
+								"gender": gender,
+								"lastname": lastname,
+								"occupation": occupation,
+								"password": hash,
+							}
+						};
+
+						console.log("Adding a new item...");
+						//put user in database
+						docClient.put(params, function(err, data) {
+							if (err) {
+								console.error("Unable to add user. Error JSON:", JSON.stringify(err, null, 2));
+							} else {
+								console.log("Added user:", JSON.stringify(data, null, 2));
+							}
+						});
+					});
+				});
+				res.redirect('/Home.html');
+			}
+			else{
+				res.redirect('/CreateAccount.html');
+			}
+		}
+	});
+});	
+
+	
 /* GET home page. */
 router.get('/CreateAccount.html', function(req, res) {
  
@@ -188,5 +253,8 @@ router.get('/FindMatch.html', function(req, res) {
  
 });
 
+var db = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+db.listTables(function(err, data) {
+console.log(data);	
 
 module.exports = router;
