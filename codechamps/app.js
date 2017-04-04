bc6d2536a49ca2b82d87ae4ca716fc698a493ac9
Siaba <siaba9@live.com>
2017-04-04 19:15:26 -0400

    Update app.js

--- codechamps/app.js
+++ codechamps/app.js
@@ -381,7 +381,8 @@ app.post('/getproblem', function(req, res){
 function getproblem(req, res){
 	var pname = req.body.problem;
 	fs.readFile('/Problems/' + pname + '.txt', (err, data) => {
-		if(err){console.log('ERR: Could not find problem description file: ' + pname + 'txt', err);}
+		if(err){console.log('ERR: Could not find problem description file: ' + pname + 'txt', err);
+		       console.log(process.cwd());}
 	});
 	res.send(data);
 }
