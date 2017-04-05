70c2a0313844928060cc176e1f3766aac3db3977
Siaba <siaba9@live.com>
2017-04-05 12:48:26 -0400

    Update app.js

--- codechamps/app.js
+++ codechamps/app.js
@@ -356,7 +356,7 @@ function runSandbox(req, res){
 				run += './test';
 				break;
 			case 'python':
-				run += 'python test.py';
+				run += 'python3.5 test.py';
 				break;
 			default:
 				break;
