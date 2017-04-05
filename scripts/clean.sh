1a71092984345892d4b0db78997ddef5fe2ef010
Siaba <siaba9@live.com>
2017-04-05 11:28:04 -0400

    Update to clean.sh - cooperates with new appsec

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
-forever stop /home/ubuntu/codechamps/app.js
-cp -r /home/ubuntu/codechamps /home
-rm -r /home/ubuntu/codechamps
+cp /home/ubuntu/codechamps/app.js /home/codechamps/app.js
+rm /home/ubuntu/codechamps/app.js
+cp -r /home/ubuntu/codechamps/webapp /home/codechamps/webapp
+rm -r /home/ubuntu/codechamps/webapp
+cp -r /home/ubuntu/codechamps/routes /home/codechamps/routes
+rm -r /home/ubuntu/codechamps/routes
+cp -r /home/ubuntu/codechamps/Problems /home/codechamps/Problems
+rm -r /home/ubuntu/codechamps/Problems
+cp -r /home/ubuntu/scripts /home/scripts
+rm -r /home/ubuntu/scripts
