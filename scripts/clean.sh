459cd695b89761afc07a1c90509d4bdb55913645
Siaba <siaba9@live.com>
2017-04-05 12:04:53 -0400

    Update clean.sh

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
-rm -r /home/codechamps/app.js
-cp -r /home/ubuntu/codechamps/app.js /home/codechamps/
-rm -r /home/ubuntu/codechamps/app.js
-rm -r /home/codechamps/webapp
-cp -r /home/ubuntu/codechamps/webapp /home/codechamps/webapp
-rm -r /home/ubuntu/codechamps/webapp
-rm -r /home/codechamps/routes
-cp -r /home/ubuntu/codechamps/routes /home/codechamps/routes
-rm -r /home/ubuntu/codechamps/routes
-rm -r /home/codechamps/Problems
-cp -r /home/ubuntu/codechamps/Problems /home/codechamps/Problems
-rm -r /home/ubuntu/codechamps/Problems
-rm -r /home/scripts
-cp -r /home/ubuntu/scripts /home/scripts
-rm -r /home/ubuntu/scripts
+cp -r /home/ubuntu/codechamps /home
+rm -r /home/codechamps
+#rm -r /home/codechamps/app.js
+#cp -r /home/ubuntu/codechamps/app.js /home/codechamps/
+#rm -r /home/ubuntu/codechamps/app.js
+#rm -r /home/codechamps/webapp
+#cp -r /home/ubuntu/codechamps/webapp /home/codechamps/webapp
+#rm -r /home/ubuntu/codechamps/webapp
+#rm -r /home/codechamps/routes
+#cp -r /home/ubuntu/codechamps/routes /home/codechamps/routes
+#rm -r /home/ubuntu/codechamps/routes
+#rm -r /home/codechamps/Problems
+#cp -r /home/ubuntu/codechamps/Problems /home/codechamps/Problems
+#rm -r /home/ubuntu/codechamps/Problems
+#rm -r /home/scripts
+#cp -r /home/ubuntu/scripts /home/scripts
+#rm -r /home/ubuntu/scripts
