9aa2ef588fe59268838346ac18046d1fd56db716
spacedmaster <spacedmaster@gmail.com>
2017-04-15 18:09:58 -0400

    Update game.js

--- codechamps/game.js
+++ codechamps/game.js
@@ -1,10 +1,36 @@
 var io;
 
-exports.test = 5;
-
 exports.initGame = function(siolib, socket){
         io = siolib;
        
         console.log("binding events"); 
+        io.on('playerJoined', playerJoined);
+        io.on('submitCode', submitCode);
+        io.on('newGame', newGame);
+        io.on('createGame', createGame);
+        io.on('playerLeft', playerLeft);
 }
     
+
+function addMatch(lang, roomID, player1ID){
+        matches.lang[roomID] = new GameInfo(player1ID);
+}
+
+//stores references to all of the game objects and matches going on
+//to add new language put lang: [] when running new version of app
+var matches = {
+  java: [],
+  c_cpp: [],
+  python: []
+};
+
+
+
+function GameInfo(pid){
+        this.p1Score = 0;
+        this.p2Score = 0;
+        this.p1ID = pid;
+        this.p2ID = "";
+        this.timeRemaining = 3000 * 60;
+        this.problemSet = [];
+}
