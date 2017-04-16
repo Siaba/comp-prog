0af34d96d3b5a39afe9560a6865f60e0cf5ff784
GriffTheMyth <gkepp13@gmail.com>
2017-04-16 00:13:42 -0400

    Update app.js

--- codechamps/app.js
+++ codechamps/app.js
@@ -614,6 +614,35 @@ function getproblem(req, res){
 	});
 }
 
+app.post('/loadDB', function(req, res){
+	loadDB(req, res);
+});
+
+function loadDB(req, res){
+	var db = new AWS.DynamoDB();
+	var table = "scoring";
+	
+	console.log(username);
+	var paramsgetuser = {
+		TableName:table,
+		Key : {"username" : {S: username}},
+		AttributesToGet: [ "username", "Sizzle Rank", "Wins", "Losses" ]
+	};
+	
+	db.getItem(paramsgetuser, function(err, data){
+		if(err) {
+			console.log(err);
+		}
+		else {
+			console.log(data);
+		}
+	});
+}
+
+		   
+	
+	
+
 var db = new AWS.DynamoDB({apiVersion: '2012-08-10'});
    db.listTables(function(err, data) {
    console.log(data);
