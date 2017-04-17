c30909e5ff1b5811906b4dd61aeee1a24e8ca7cc
spacedmaster <spacedmaster@gmail.com>
2017-04-17 17:43:18 -0400

    Update Player.js

--- codechamps/webapp/Player.js
+++ codechamps/webapp/Player.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
 	    $("#sid").val(data.sid);
 	    console.log(data.uname + "" + data.sid);
     });
-    $("#findmatch").submit(function(e){
+    $("#findmatchform").submit(function(e){
 	   e.preventDefault();
 	   var sid = $('#sid');
 	   var uname = $('#uname');
