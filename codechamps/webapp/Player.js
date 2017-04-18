d7e5f9dd13e4c775e8c23add70de0f1432a47b82
spacedmaster <spacedmaster@gmail.com>
2017-04-17 23:09:42 -0400

    Update Player.js

--- codechamps/webapp/Player.js
+++ codechamps/webapp/Player.js
@@ -21,17 +21,18 @@ $(document).ready(function(){
 function initSocket(){
     clientSocket = io.connect();
     clientSocket.on('connect', function(){
-	    clientSocket.emit('join_room', "Joining room ");
+	    
 	    
 	});
 }
 
 function bindEvents(){
 	clientSocket.on('playerJoined', playerJoined);
+	
 	console.log("binding events");
 }
 
 function playerJoined(){
 	console.log("Player joined event");
-	clientSocket.emit('hello', "Hi i am a socket");	
+	
 }
