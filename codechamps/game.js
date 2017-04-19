df1d2189cfbf786d72de4d88bee7eb7f6e36d0aa
Siaba <siaba9@live.com>
2017-04-19 13:01:31 -0400

    Update game.js

--- codechamps/game.js
+++ codechamps/game.js
@@ -34,23 +34,16 @@ function findMatch(data){
                         //this is where you join a game instead of create one
                         joinMatch(data.lang,key,data.uname,data.sid);
                         this.join(key);
-                        fs.readFile('/tmp/box/' + sID + '/box/error.txt' , 'utf8', function (err,data) {
-						if (err) {
-							return console.log(err);
-						}
-					 	console.log(data);
-					 	callback(true,data);
-					});
+			console.log(data.uname, " (box: ", data.sid, ") joined ", data.lang, " ", key);
                         fs.readFile('/home/ubuntu/codechamps/webapp/gamepage.txt', function(err,data) {
                                 if (!err) {
                                         console.log('gamepage.txt read, sending to room...');
-                                        io.sockets.in(key).("loadGame", data);
+                                        io.sockets.in(key).emit("loadGame", data);
                                 }
                                 else{
                                         console.log(err);
                                 }
                         });
-                        console.log(data.uname, " (box: ", data.sid, ") joined ", data.lang, " ", key);
                         return;
                 }
         }
