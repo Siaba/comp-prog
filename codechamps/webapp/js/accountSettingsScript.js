80312342009c212d75fe233f8d009a0ae3c00041
nohainnogain <nkh5046@psu.edu>
2017-04-22 16:22:53 -0400

    Update accountSettingsScript.js

--- codechamps/webapp/js/accountSettingsScript.js
+++ codechamps/webapp/js/accountSettingsScript.js
@@ -1,3 +1,64 @@
+function hiddenValue() {
+
+var optionSelected = document.getElementById("changeWhat").selectedIndex;
+
+if (optionSelected == 0) {
+  document.getElementById("old_password").type="password";
+  document.getElementById("oldPasswordLabel").className= '';
+  document.getElementById("new_password").type="password";
+  document.getElementById("newPasswordLabel").className='';
+  document.getElementById("new_password_reentered").type="password";
+  document.getElementById("reNewPasswordLabel").className='';
+  document.getElementById("oldPasswordWrong").innerHTML = '';
+  document.getElementById("newPasswordWrong").innerHTML = '';
+  document.getElementById("passwordWrong").innerHTML = '';
+  document.getElementById("oldMail").type="hidden";
+  document.getElementById("oldMailLabel").className='hidden';
+  document.getElementById("theMail").type="hidden";
+  document.getElementById("mailLabel").className='hidden';
+  document.getElementById("reMail").type="hidden";
+  document.getElementById("reMailLabel").className='hidden';
+
+}
+if (optionSelected == 1) {
+  document.getElementById("old_password").type="hidden";
+  document.getElementById("oldPasswordLabel").className= 'hidden';
+  document.getElementById("new_password").type="hidden";
+  document.getElementById("newPasswordLabel").className='hidden';
+  document.getElementById("new_password_reentered").type="hidden";
+  document.getElementById("reNewPasswordLabel").className='hidden';
+  document.getElementById("oldPasswordWrong").innerHTML = '';
+  document.getElementById("newPasswordWrong").innerHTML = '';
+  document.getElementById("passwordWrong").innerHTML = '';
+  document.getElementById("oldMail").type="email";
+  document.getElementById("oldMailLabel").className='';
+  document.getElementById("theMail").type="email";
+  document.getElementById("mailLabel").className='';
+  document.getElementById("reMail").type="email";
+  document.getElementById("reMailLabel").className='';
+
+}
+if (optionSelected == 2) {
+  document.getElementById("oldMail").type="email";
+  document.getElementById("oldMailLabel").className='';
+  document.getElementById("theMail").type="email";
+  document.getElementById("mailLabel").className='';
+  document.getElementById("reMail").type="email";
+  document.getElementById("reMailLabel").className='';
+  document.getElementById("usernameWrong").innerHTML = '';
+  document.getElementById("old_password").type="password";
+  document.getElementById("oldPasswordLabel").className= '';
+  document.getElementById("new_password").type="password";
+  document.getElementById("newPasswordLabel").className='';
+  document.getElementById("new_password_reentered").type="password";
+  document.getElementById("reNewPasswordLabel").className='';
+  document.getElementById("oldPasswordWrong").innerHTML = '';
+  document.getElementById("newPasswordWrong").innerHTML = '';
+  document.getElementById("passwordWrong").innerHTML = '';
+
+}
+}
+
         function checkFirstEmail() {
           var getEmail = document.getElementById("oldMail");
           var eMail = getEmail.value;
@@ -104,37 +165,12 @@
               }
             }
 
-          function validAge() {
-            var getAge = document.getElementById("userAge");
-            var age = getAge.value;
-            var text = "";
-            if(age < 0 || age > 110) {
-              text = "* Please enter your real age.";
-              text = text.fontcolor("red");
-              document.getElementById("ageWrong").innerHTML = text;
-            }
-            else if(age =='') {
-              text = "* Please enter an age.";
-              text = text.fontcolor("red");
-              document.getElementById("ageWrong").innerHTML = text;
-            }
-            else if(isNaN(age)) {
-              text = "* Please enter your age as a number.";
-              text = text.fontcolor("red");
-              document.getElementById("ageWrong").innerHTML = text;
-            }
-            else {
-              document.getElementById("ageWrong").innerHTML = text;
-            }
-          }
-
           function validUsername() {
             var getUsername = document.getElementById("oldUsername");
             var userName = getUsername.value;
-            var newUsername = document.getElementById("newUsername").value;
             var usernameRegexpression = /^[a-zA-Z0-9]+$/;
             var text = "";
-            if (userName.length == 0 || newUsername.length > 0) {
+            if (userName.length == 0 ){
               text = "* Please enter your old username.";
               text = text.fontcolor("red");
               document.getElementById("usernameWrong").innerHTML = text;
@@ -151,31 +187,6 @@
             }
             else {
               document.getElementById("usernameWrong").innerHTML = text;
-            }
-          }
-
-          function validUsernameNew() {
-            var getUsername = document.getElementById("newUsername");
-            var newUserName = getUsername.value;
-            var usernameRegexpression = /^[a-zA-Z0-9]+$/;
-            var text = "";
-            if (newUserName.length == 0) {
-              text = "* Please enter a new username.";
-              text = text.fontcolor("red");
-              document.getElementById("newUsernameWrong").innerHTML = text;
-            }
-            else if (newUserName.length < 6) {
-              text = "* Please enter a new username containing at least 6 characters.";
-              text = text.fontcolor("red");
-              document.getElementById("newUsernameWrong").innerHTML = text;
-            }
-            else if (!newUserName.match(usernameRegexpression)) {
-              text = "* Illegal character(s) detected. Please use only letters and numbers for your username.";
-              text = text.fontcolor("red");
-              document.getElementById("newUsernameWrong").innerHTML = text;
-            }
-            else {
-              document.getElementById("newUsernameWrong").innerHTML = text;
             }
           }
 
@@ -246,116 +257,3 @@
          }
       }*/
 
-function hiddenValue() {
-var optionSelected = document.getElementById("changeWhat").selectedIndex;
-if (optionSelected == 0) {
-  document.getElementById("oldUsername").type = "text";
-  document.getElementById("oldUsernameLabel").className = '';
-  document.getElementById("newUsername").type="text";
-  document.getElementById("newUsernameLabel").className = '';
-  document.getElementById("usernameWrong").innerHTML = '';
-  document.getElementById("newUsernameWrong").innerHTML = '';
-  document.getElementById("old_password").type="hidden";
-  document.getElementById("oldPasswordLabel").className= 'hidden';
-  document.getElementById("new_password").type="hidden";
-  document.getElementById("newPasswordLabel").className='hidden';
-  document.getElementById("new_password_reentered").type="hidden";
-  document.getElementById("reNewPasswordLabel").className='hidden';
-  document.getElementById("oldMail").type="hidden";
-  document.getElementById("oldMailLabel").className='hidden';
-  document.getElementById("theMail").type="hidden";
-  document.getElementById("mailLabel").className='hidden';
-  document.getElementById("reMail").type="hidden";
-  document.getElementById("reMailLabel").className='hidden';
-
-}
-if (optionSelected == 1) {
-  document.getElementById("oldUsername").type="hidden";
-  document.getElementById("oldUsernameLabel").className = 'hidden';
-  document.getElementById("newUsername").type="hidden";
-  document.getElementById("newUsernameLabel").className = 'hidden';
-  document.getElementById("old_password").type="password";
-  document.getElementById("oldPasswordLabel").className= '';
-  document.getElementById("new_password").type="password";
-  document.getElementById("newPasswordLabel").className='';
-  document.getElementById("new_password_reentered").type="password";
-  document.getElementById("reNewPasswordLabel").className='';
-  document.getElementById("oldPasswordWrong").innerHTML = '';
-  document.getElementById("newPasswordWrong").innerHTML = '';
-  document.getElementById("passwordWrong").innerHTML = '';
-  document.getElementById("oldMail").type="hidden";
-  document.getElementById("oldMailLabel").className='hidden';
-  document.getElementById("theMail").type="hidden";
-  document.getElementById("mailLabel").className='hidden';
-  document.getElementById("reMail").type="hidden";
-  document.getElementById("reMailLabel").className='hidden';
-
-}
-if (optionSelected == 2) {
-  document.getElementById("oldUsername").type="hidden";
-  document.getElementById("oldUsernameLabel").className = 'hidden';
-  document.getElementById("newUsername").type="hidden";
-  document.getElementById("newUsernameLabel").className = 'hidden';
-  document.getElementById("old_password").type="hidden";
-  document.getElementById("oldPasswordLabel").className= 'hidden';
-  document.getElementById("new_password").type="hidden";
-  document.getElementById("newPasswordLabel").className='hidden';
-  document.getElementById("new_password_reentered").type="hidden";
-  document.getElementById("reNewPasswordLabel").className='hidden';
-  document.getElementById("oldMail").type="email";
-  document.getElementById("oldMailLabel").className='';
-  document.getElementById("theMail").type="email";
-  document.getElementById("mailLabel").className='';
-  document.getElementById("reMail").type="email";
-  document.getElementById("reMailLabel").className='';
-
-}
-if (optionSelected == 3) {
-  document.getElementById("oldMail").type="hidden";
-  document.getElementById("oldMailLabel").className='hidden';
-  document.getElementById("theMail").type="hidden";
-  document.getElementById("mailLabel").className='hidden';
-  document.getElementById("reMail").type="hidden";
-  document.getElementById("reMailLabel").className='hidden';
-  document.getElementById("oldUsername").type = "text";
-  document.getElementById("oldUsernameLabel").className = '';
-  document.getElementById("newUsername").type="text";
-  document.getElementById("newUsernameLabel").className = '';
-  document.getElementById("usernameWrong").innerHTML = '';
-  document.getElementById("newUsernameWrong").innerHTML = '';
-  document.getElementById("old_password").type="password";
-  document.getElementById("oldPasswordLabel").className= '';
-  document.getElementById("new_password").type="password";
-  document.getElementById("newPasswordLabel").className='';
-  document.getElementById("new_password_reentered").type="password";
-  document.getElementById("reNewPasswordLabel").className='';
-  document.getElementById("oldPasswordWrong").innerHTML = '';
-  document.getElementById("newPasswordWrong").innerHTML = '';
-  document.getElementById("passwordWrong").innerHTML = '';
-
-}
-if (optionSelected == 4) {
-  document.getElementById("oldUsername").type = "text";
-  document.getElementById("oldUsernameLabel").className = '';
-  document.getElementById("newUsername").type="text";
-  document.getElementById("newUsernameLabel").className = '';
-  document.getElementById("usernameWrong").innerHTML = '';
-  document.getElementById("newUsernameWrong").innerHTML = '';
-  document.getElementById("old_password").type="password";
-  document.getElementById("oldPasswordLabel").className= '';
-  document.getElementById("new_password").type="password";
-  document.getElementById("newPasswordLabel").className='';
-  document.getElementById("new_password_reentered").type="password";
-  document.getElementById("reNewPasswordLabel").className='';
-  document.getElementById("oldPasswordWrong").innerHTML = '';
-  document.getElementById("newPasswordWrong").innerHTML = '';
-  document.getElementById("passwordWrong").innerHTML = '';
-  document.getElementById("oldMail").type="email";
-  document.getElementById("oldMailLabel").className='';
-  document.getElementById("theMail").type="email";
-  document.getElementById("mailLabel").className='';
-  document.getElementById("reMail").type="email";
-  document.getElementById("reMailLabel").className='';
-
-}
-}
