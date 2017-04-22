572d8994f35ad31f07fed839e10d63a873d6f8d8
spacedmaster <spacedmaster@gmail.com>
2017-04-22 14:19:23 -0400

    Update game.js

--- codechamps/game.js
+++ codechamps/game.js
@@ -13,17 +13,19 @@ exports.initGame = function(siolib, socket){
 }
     
 
-function addMatch(lang, roomID, player1ID, player1BoxID){
+function addMatch(lang, roomID, player1ID, player1BoxID, player1SocketID){
         console.log('Room id: ' + roomID + ', lang: ' + lang + 'playerID ' + player1ID );
-        matches[lang][roomID] = new GameInfo(player1ID, player1BoxID);
+        matches[lang][roomID] = new GameInfo(player1ID, player1BoxID, player1SocketID);
+	
         //this.join(roomID);
         //console.log("game info: " + matches[lang][roomID].p1ID + " " + matches[lang][roomID].p1BoxID);
         //emit an event to the client that created the match so their browser can update screen
 }
 
-function joinMatch(lang, roomID, player2ID, player2BoxID){
+function joinMatch(lang, roomID, player2ID, player2BoxID, player2SocketID){
         matches[lang][roomID].p2ID = player2ID;
         matches[lang][roomID].p2BoxID = player2BoxID;
+	matches[lang][roomID].p2SocketID = player2SocketID;
         matches[lang][roomID].numPlayers++;
         //this.join(roomID);
         //emit join event to client
@@ -33,7 +35,7 @@ function findMatch(data){
         for(var key in matches[data.lang]){
                 if(matches[data.lang][key].numPlayers < 2){
                         //this is where you join a game instead of create one
-                        joinMatch(data.lang,key,data.uname,data.sid);
+                        joinMatch(data.lang,key,data.uname,data.sid, this.id);
                         this.join(key);
 			console.log(data.uname, " (box: ", data.sid, ") joined ", data.lang, " ", key);
                         fs.readFile('/home/ubuntu/codechamps/webapp/gamepage.txt', function(err,data) {
@@ -49,7 +51,7 @@ function findMatch(data){
                 }
         }
         var roomID = ( Math.random() * 100000 ) | 0;
-        addMatch(data.lang, roomID, data.uname, data.sid);
+        addMatch(data.lang, roomID, data.uname, data.sid, this.id);
         this.join(roomID);
         console.log(data.uname, " (box: ", data.sid, ") created ", data.lang, " ", roomID);
         return;
@@ -81,9 +83,11 @@ var matches = {
 
 
 
-function GameInfo(pid, pBoxid){
+function GameInfo(pid, pBoxid, pSockID){
         this.p1Score = 0;
         this.p2Score = 0;
+	this.p1SocketID = pSockID;
+	this.p2SocketID = "";
         this.p1ID = pid;
         this.p2ID = "";
         this.p1BoxID = pBoxid;
@@ -91,4 +95,5 @@ function GameInfo(pid, pBoxid){
         this.timeRemaining = 3000 * 60;
         this.problemSet = [];
         this.numPlayers = 1;
+	this.readyPlayers = 0;
 }
